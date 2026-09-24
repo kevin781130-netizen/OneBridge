4af7c72128d01a994f0780b861426f2e6df3b1ff
kevin781130-netizen <kevin781130@gmail.com>
2026-09-24 23:51:20 +0800

    refactor: export real adapter implementations

--- src/onebridge/adapters/__init__.py
+++ src/onebridge/adapters/__init__.py
@@ -1,4 +1,14 @@
 from .base import AdapterRegistry, OneBridgeAdapter
+from .flowise import FlowisePredictionAdapter
+from .hermes import HermesCommandAdapter
 from .mock import default_mock_registry
+from .open_design import OpenDesignMCPAdapter
 
-__all__ = ["AdapterRegistry", "OneBridgeAdapter", "default_mock_registry"]
+__all__ = [
+    "AdapterRegistry",
+    "OneBridgeAdapter",
+    "FlowisePredictionAdapter",
+    "OpenDesignMCPAdapter",
+    "HermesCommandAdapter",
+    "default_mock_registry",
+]
