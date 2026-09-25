5d1088dca8b0a368b93573f9eacf35f0828edec9
kevin781130-netizen <kevin781130@gmail.com>
2026-09-25 21:38:55 +0800

    fix: accept governance context in release test controllers

--- tests/test_two_person_release.py
+++ tests/test_two_person_release.py
@@ -43,7 +43,7 @@ class Controller:
         self.deployments = DeploymentSwitchService(db)
         self.calls = []
 
-    def run(self, slot, adapters):
+    def run(self, slot, adapters, *, governance=None):
         self.calls.append((slot, tuple(adapters)))
         return {
             "release_id": "rel_core",
